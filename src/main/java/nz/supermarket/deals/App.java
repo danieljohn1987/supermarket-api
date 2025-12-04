e449414337ff11a766efacc370c31691761e1c07
Daniel <daniel@Daniel-Laptop.localdomain>
2025-12-05 00:55:04 +1300

    feat: scaffold Maven project via Docker archetype

---
+++ src/main/java/nz/supermarket/deals/App.java
@@ -0,0 +1,13 @@
+package nz.supermarket.deals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/nz/supermarket/deals/AppTest.java
@@ -0,0 +1,20 @@
+package nz.supermarket.deals;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
